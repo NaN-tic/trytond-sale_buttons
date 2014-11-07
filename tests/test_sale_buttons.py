749ea0344231537decbf7dcb85e830f66647c736
jmartin <jmartin@zikzakmedia.com>
2014-11-07 14:36:01 +0100

    Cleanup tests

--- tests/test_sale_buttons.py
+++ tests/test_sale_buttons.py
@@ -1,24 +1,14 @@
 #!/usr/bin/env python
-#This file is part sale_buttons module for Tryton.
-#The COPYRIGHT file at the top level of this repository contains 
-#the full copyright notices and license terms.
-
-import sys
-import os
-DIR = os.path.abspath(os.path.normpath(os.path.join(__file__,
-    '..', '..', '..', '..', '..', 'trytond')))
-if os.path.isdir(DIR):
-    sys.path.insert(0, os.path.dirname(DIR))
-
+# This file is part sale_buttons module for Tryton.
+# The COPYRIGHT file at the top level of this repository contains
+# the full copyright notices and license terms.
 import unittest
 import trytond.tests.test_tryton
 from trytond.tests.test_tryton import test_view, test_depends
 
 
 class SaleButtonsTestCase(unittest.TestCase):
-    '''
-    Test Sale Buttons module.
-    '''
+    'Test Sale Buttons module'
 
     def setUp(self):
         trytond.tests.test_tryton.install_module('sale_buttons')
@@ -28,9 +18,7 @@ class SaleButtonsTestCase(unittest.TestCase):
         test_view('sale_buttons')
 
     def test0006depends(self):
-        '''
-        Test depends.
-        '''
+        'Test depends'
         test_depends()
 
 
